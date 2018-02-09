042780120bfbd037ee79ef705ed49b33eb7f7974
SimoneGrant <simonegrant001@gmail.com>
2018-02-09 11:05:35 -0500

    Storyboard and pod setup

---
+++ iPlan/View Controller/CategoriesTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iPlan
+//
+//  Created by Simone Grant on 2/9/18.
+//  Copyright © 2018 Simone Grant. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
---
+++ iPlan/View Controller/SwipeTableViewController.swift
@@ -0,0 +1,52 @@
+//
+//  SwipeTableViewController.swift
+//  iPlan
+//
+//  Created by Simone Grant on 2/9/18.
+//  Copyright © 2018 Simone Grant. All rights reserved.
+//
+
+import UIKit
+import SwipeCellKit
+import RealmSwift
+
+class SwipeTableViewController: UITableViewController, SwipeTableViewCellDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.rowHeight = 80.0
+        tableView.separatorStyle = .none
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell", for: indexPath) as! SwipeTableViewCell
+        cell.delegate = self
+        return cell
+    }
+    
+    //MARK: - Swipe Cell Delegate Methods
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
+        guard orientation == .right else { return nil }
+        let deleteAction = SwipeAction(style: .destructive, title: "Delete") { (action, indexPath) in
+            self.updateModel(at: indexPath)
+        }
+        //customize the action appearance
+        deleteAction.image = UIImage(named: "delete-icon-1.png")
+        return [deleteAction]
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeTableOptions {
+        var options = SwipeTableOptions()
+        options.expansionStyle = .destructive
+        return options
+    }
+    
+    func updateModel(at indexPath: IndexPath) {
+        //Update our data model
+    }
+
+}
